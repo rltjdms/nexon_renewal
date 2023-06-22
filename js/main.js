74ce023169bf3c7676212fba12135815d7f969da
hoon95 <gmline_@naver.com>
2023-06-22 23:41:45 +0900

    Feat: 슬라이드 기능 구현 수정

--- js/main.js
+++ js/main.js
@@ -8,6 +8,7 @@ let slideWrapper = document.querySelector('.slidewrapper'),                  // 
     slideContainer = slideWrapper.querySelector('ul'),                    // ul
     slides = slideContainer.querySelectorAll('li'),                    // 각 슬라이드
     slideCount = slides.length,                    // 슬라이드 개수
+    slideWidth = slides[0].offsetWidth,
     currentIndex = 0,                   // 현재 보고있는 화면
     prevBtn = slideWrapper.querySelector('.prev'),
     nextBtn = slideWrapper.querySelector('.next');
@@ -16,39 +17,31 @@ let slideWrapper = document.querySelector('.slidewrapper'),                  // 
 // 슬라이드 가로배치
 
 // slideContainer의 너비 지정
-slideContainer.style.width = `${slideWrapper.offsetWidth * slideCount}px`;  // ul의 높이가 안나오므로 cf부여
+slideContainer.style.width = `${slides[0].offsetWidth * slideCount}px`;  // ul의 높이가 안나오므로 cf부여
 
 // 슬라이드 이동 함수 moveSlide
+// function moveSlide(num){
+//     slideContainer.style.left=`${-100*num}%`;
+//     currentIndex=num;
+// }
+
 function moveSlide(num){
-    slideContainer.style.left=`${-100*num}%`;
+    // slideContainer.style.left=`${-100*num}%`;
+    // slideContainer.style.left=`${-slideWidth*num}px`;
+    slideContainer.style.left=`${-slideWidth*num}px`;
+
     currentIndex=num;
 }
 
-// 좌우 버튼으로 슬라이드 이동
-nextBtn.addEventListener('click',e=>{
-    e.preventDefault();         // 링크의 기본속성을 막자
-    if(currentIndex<slideCount-1){
-        moveSlide(currentIndex+1);
-    }else{
-        moveSlide(0);
-    }
-});
-prevBtn.addEventListener('click',e=>{
+nextBtn.addEventListener('click', e=>{
     e.preventDefault();
-    if(currentIndex>0){
-        moveSlide(currentIndex-1);
-    }else{
-        moveSlide(slideCount-1);
-    }
+    // moveSlide(Math.floor(slideCount/4));
+    moveSlide(1)
+});
+prevBtn.addEventListener('click', e=>{
+    e.preventDefault();
+    // moveSlide(Math.floor(slideCount/4-1));
+    moveSlide(0)
 });
 
 let qnaList = document.querySelectorAll('.qna_list li');
-
-qnaList.forEach(item => {
-    item.addEventListener('click', e=>{
-        qnaList.forEach(items=>{
-            items.classList.remove('active');
-        })
-        e.currentTarget.classList.add('active');
-    })
-}); 
