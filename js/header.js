5b997185d487daca354c898d02a94740bd0e456b
rltjdms <euni48730@gmail.com>
2023-06-22 18:24:51 +0900

    Feat: 스크립트 추가 및 CSS 수정 vs2.1

--- js/header.js
+++ js/header.js
@@ -1,14 +1,23 @@
 let header = document.querySelector("header"),
+    headerSub = document.querySelectorAll("header_submenu"),
+    headerSubOST = headerSub.offsetHeight,
     Nav = header.querySelector("nav"),
     NavMenu = Nav.querySelectorAll("nav > ul > li");
 
      NavMenu.forEach(item => {
       item.addEventListener("mouseenter", (e) => {
           e.preventDefault();
+          let SubMenuHeight =
+              e.Target.querySelector("ul").offsetHeight;
+          let totalHeight = headerHeight + SubMenuHeight + 30;
+          header.style.height = `${totalHeight}px`;
+
           item.classList.add("on");
       });
       item.addEventListener("mouseleave", (e) => {
           e.preventDefault();
+          header.style.height = `${headerHeight}px`;
+
           item.classList.remove("on");
       });
     });
