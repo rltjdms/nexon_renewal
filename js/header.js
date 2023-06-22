50c7378d37ed5e592d836997adb8283907f88d13
rltjdms <euni48730@gmail.com>
2023-06-22 16:39:56 +0900

    Feat: 스크립트 추가 및 CSS 수정 vs2.0

--- js/header.js
+++ js/header.js
@@ -1,15 +1,15 @@
-let header = document.querySelector('header'),
-    Nav = header.querySelector('nav'),
-    NavMenu = Nav.querySelectorAll('nav > ul > li'),
-    NavSubmenu = Nav.querySelectorAll('.header_submenu');
+let header = document.querySelector("header"),
+    Nav = header.querySelector("nav"),
+    NavMenu = Nav.querySelectorAll("nav > ul > li");
 
      NavMenu.forEach(item => {
       item.addEventListener("mouseenter", (e) => {
           e.preventDefault();
-          NavSubmenu.classList.add("on");
+          item.classList.add("on");
       });
       item.addEventListener("mouseleave", (e) => {
           e.preventDefault();
-          NavSubmenu.classList.remove("on");
+          item.classList.remove("on");
       });
     });
+    // Nav에 마우스를 올리면 서브메뉴 나옴
