4a64c71bc5546ec6d8bac80615d2f0654c27aeab
rltjdms <euni48730@gmail.com>
2023-06-26 12:30:43 +0900

    Fix: css, html, scriptr 수정

--- js/header.js
+++ js/header.js
@@ -3,22 +3,34 @@ let header = document.querySelector("header"),
     NavMenu = Nav.querySelectorAll("nav > ul > li"),
     headerHeight = header.offsetHeight;
 
-     NavMenu.forEach(item => {
-      item.addEventListener("mouseenter", (e) => {
-          e.preventDefault();
-          item.classList.add("on");
-          
-          let SubMenuHeight = e.currentTarget.querySelector('ul').offsetHeight;
-          let totalHeight = headerHeight + SubMenuHeight + 30;
-          header.style.height = `${totalHeight}px`;
+NavMenu.forEach((item) => {
+    item.addEventListener("mouseenter", (e) => {
+        e.preventDefault();
+        item.classList.add("on");
 
-          item.classList.add("on");
-      });
-      item.addEventListener("mouseleave", (e) => {
-          e.preventDefault();
-          item.classList.remove("on");
+        let SubMenuHeight = e.currentTarget.querySelector("ul").offsetHeight;
+        let headerSubheight =
+            header.querySelector(".header_submenu").offsetHeight;
+        let totalHeight = headerHeight + SubMenuHeight + headerSubheight + 30;
+        header.style.height = `${totalHeight}px`;
 
-          header.style.height = `${headerHeight}px`;     
-      });
+        item.classList.add("on");
     });
-    // Nav에 마우스를 올리면 서브메뉴 나옴
+    item.addEventListener("mouseleave", (e) => {
+        e.preventDefault();
+        item.classList.remove("on");
+
+        header.style.height = `${headerHeight}px`;
+    });
+});
+// Nav에 마우스를 올리면 서브메뉴 나옴
+
+
+// 스크롤시
+window.addEventListener("scroll", ()=> {
+    if(window.scrollY > 0) {
+        header.classList.add("active");
+    } else {
+        header.classList.remove("active");
+    }
+})
