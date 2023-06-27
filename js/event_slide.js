14eacca95042e2c165246cbba03ff4b4633aa70d
Aaaruem <137126436+Aaaruem@users.noreply.github.com>
2023-06-27 15:48:04 +0900

    event js write commit

---
+++ js/event_slide.js
@@ -0,0 +1,7 @@
+// event pager 시작
+let eventbox = document.querySelector(".event"), 
+    eventslide = eventbox.querySelector(".event-slide"), 
+    slides = eventslide.querySelectorAll("li"), 
+    slideCount = slides.length, //슬라이드개수
+
+
