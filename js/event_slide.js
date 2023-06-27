bdc6144d9662d05139dc0fba8dda6d5efddecca9
Aaaruem <137126436+Aaaruem@users.noreply.github.com>
2023-06-27 17:32:07 +0900

    event js write

--- js/event_slide.js
+++ js/event_slide.js
@@ -1,7 +1,14 @@
 // event pager 시작
-let eventbox = document.querySelector(".event"), 
-    eventslide = eventbox.querySelector(".event-slide"), 
-    slides = eventslide.querySelectorAll("li"), 
-    slideCount = slides.length, //슬라이드개수
+let eventslide = document.querySelector("event-slide"), //ul
+  eventslideList = eventslide.querySelectorAll("li"), //li
+  eventDot = eventslide.querySelector(".event-dot");
+currentIndex = 0; //현재이미지
+slideCount = slides.length; // 이미지개수
+dotIndex = "";
 
-
+//dot 클릭했을 때 이미지 이동
+eventslide.querySelectorAll(".event_dot").forEach((dot, index) => {
+  dot.addEventListener("click", () => {
+    gotoSlider(index);
+  });
+});
