f9d84bafd06fdb98d1ff1b988c7b48acdf25abc5
Aaaruem <137126436+Aaaruem@users.noreply.github.com>
2023-06-28 19:30:17 +0900

    event_slide.js수정

--- js/event_slide.js
+++ js/event_slide.js
@@ -14,7 +14,7 @@ let eventWrapper = document.querySelector(".event"), // event(부모)
   dotPager = eventWrapper.querySelector(".event-dot"), //dot버튼
   dotIndex = "",
   eventTimer,
-  slideCount = slides.length,
+  slideCount = eventslideList.length,
   currentSlideIdx = 0;
 
 if (slideCount > 1) {
@@ -28,27 +28,27 @@ if (slideCount > 1) {
 dotPager.innerHTML = dotIndex;
 let dotbtn = dotPager.querySelectorAll("a");
 
-function moveSlide(num) {
-  slideContainer.style.left = `${-num * 100}%`;
+function eventmoveSlide(num) {
+  eventSlide.style.left = `${-num * 100}%`;
   currentSlideIdx = num;
   console.log(currentSlideIdx);
 
-  for (let sl of slides) {
-    sl.classList.remove("active");
+  for (let es of eventslideList) {
+    es.classList.remove("active");
   }
   eventslideList[currentSlideIdx].classList.add("active");
-  //모든 페이저에서 active를 제거하고, 현재 슬라이드번호의 pager에 active 추가
+  // 모든 페이저에서 active를 제거하고, 현재 슬라이드번호의 pager에 active 추가
   for (let db of dotbtn) {
-    pb.classList.remove("active");
+    db.classList.remove("active");
   }
   dotbtn[currentSlideIdx].classList.add("active");
 } //moveslide
-moveSlide(0);
+eventmoveSlide(0);
 
 dotbtn.forEach((item, idx) => {
   item.addEventListener("click", (e) => {
     e.preventDefault();
-    moveSlide(idx);
+    eventmoveSlide(idx);
   });
 });
 
@@ -56,13 +56,13 @@ function autoSlide() {
   eventTimer = setInterval(() => {
     //let nextIdx = currentSlideIdx + 1;
     let nextIdx = (currentSlideIdx + 1) % slideCount;
-    moveSlide(nextIdx);
+    eventmoveSlide(nextIdx);
   }, 3000);
 }
 autoSlide();
 
 eventWrapper.addEventListener("mouseenter", () => {
-  clearInterval(timer);
+  clearInterval(eventTimer);
 });
 eventWrapper.addEventListener("mouseleave", () => {
   autoSlide();
--- js/main.js
+++ js/main.js
@@ -4,15 +4,15 @@
     그 스크롤양이 0보다 크면 header에 sticky 추가
     아니면 sticky 제거
 */
-let header = document.querySelector('body');       // 다른데 쓸 수도 있음
+// let header = document.querySelector("body"); // 다른데 쓸 수도 있음
 
-window.addEventListener('scroll', ()=>{
-    if(window.scrollY > 0){
-        header.classList.add('sticky');
-    }else{
-        header.classList.remove('sticky');
-    }
-});
+// window.addEventListener("scroll", () => {
+//   if (window.scrollY > 0) {
+//     header.classList.add("sticky");
+//   } else {
+//     header.classList.remove("sticky");
+//   }
+// });
 
 /* 슬라이드 */
 /*
@@ -20,14 +20,13 @@ window.addEventListener('scroll', ()=>{
     마지막 슬라이드에서다음 버튼을 클릭하면 첫 슬라이드로 이동
 */
 
-let slideWrapper = document.querySelector('.slidewrapper'),                  // ul의 부모
-    slideContainer = slideWrapper.querySelector('ul'),                    // ul
-    slides = slideContainer.querySelectorAll('li'),                    // 각 슬라이드
-    slideCount = slides.length,                    // 슬라이드 개수
-    currentIndex = 0,                   // 현재 보고있는 화면
-    prevBtn = slideWrapper.querySelector('.prev'),
-    nextBtn = slideWrapper.querySelector('.next');
-
+// let slideWrapper = document.querySelector('.slidewrapper'),                  // ul의 부모
+//     slideContainer = slideWrapper.querySelector('ul'),                    // ul
+//     slides = slideContainer.querySelectorAll('li'),                    // 각 슬라이드
+//     slideCount = slides.length,                    // 슬라이드 개수
+//     currentIndex = 0,                   // 현재 보고있는 화면
+//     prevBtn = slideWrapper.querySelector('.prev'),
+//     nextBtn = slideWrapper.querySelector('.next');
 
 // 슬라이드 가로배치
 
@@ -38,31 +37,31 @@ slides.forEach((item,index)=>{
 
 // 2번 방법
 // slideContainer의 너비 지정
-slideContainer.style.width = `${slideWrapper.offsetWidth * slideCount}px`;  // ul의 높이가 안나오므로 cf부여
+// slideContainer.style.width = `${slideWrapper.offsetWidth * slideCount}px`; // ul의 높이가 안나오므로 cf부여
 
 // 슬라이드 이동 함수 moveSlide
-function moveSlide(num){
-    slideContainer.style.left=`${-100*num}%`;
-    currentIndex=num;
-}
+// function moveSlide(num) {
+//   slideContainer.style.left = `${-100 * num}%`;
+//   currentIndex = num;
+// }
 
 // 좌우 버튼으로 슬라이드 이동
-nextBtn.addEventListener('click',(e)=>{
-    e.preventDefault();         // 링크의 기본속성을 막자
-    if(currentIndex<slideCount-1){
-        moveSlide(currentIndex+1);
-    }else{
-        moveSlide(0);
-    }
-});
-prevBtn.addEventListener('click',(e)=>{
-    e.preventDefault();
-    if(currentIndex>0){
-        moveSlide(currentIndex-1);
-    }else{
-        moveSlide(slideCount-1);
-    }
-});
+// nextBtn.addEventListener("click", (e) => {
+//   e.preventDefault(); // 링크의 기본속성을 막자
+//   if (currentIndex < slideCount - 1) {
+//     moveSlide(currentIndex + 1);
+//   } else {
+//     moveSlide(0);
+//   }
+// });
+// prevBtn.addEventListener("click", (e) => {
+//   e.preventDefault();
+//   if (currentIndex > 0) {
+//     moveSlide(currentIndex - 1);
+//   } else {
+//     moveSlide(slideCount - 1);
+//   }
+// });
 
 /* 
 변수명 qnaList에 qna_list li를 지정
@@ -71,13 +70,13 @@ qnaList를 클릭하면 할일
     클릭한 그 요소에만 active 추가
 */
 
-let qnaList = document.querySelectorAll('.qna_list li');
+// let qnaList = document.querySelectorAll(".qna_list li");
 
-qnaList.forEach(item => {
-    item.addEventListener('click', e=>{
-        qnaList.forEach(items=>{
-            items.classList.remove('active');
-        })
-        e.currentTarget.classList.add('active');
-    })
-}); 
+// qnaList.forEach((item) => {
+//   item.addEventListener("click", (e) => {
+//     qnaList.forEach((items) => {
+//       items.classList.remove("active");
+//     });
+//     e.currentTarget.classList.add("active");
+//   });
+// });
