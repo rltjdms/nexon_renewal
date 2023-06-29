69da448ecad1bd1eca127629ba9a8a56c1e09352
hoon95 <gmline_@naver.com>
2023-06-29 17:18:09 +0900

    Feat: 게임 수에 따른 슬라이드 버튼 안보이게 하기

--- js/main.js
+++ js/main.js
@@ -1,64 +1,20 @@
-let header = document.querySelector("header"),
-    Nav = header.querySelector("nav"),
-    NavMenu = Nav.querySelectorAll("nav > ul > li");
-// headerHeight = header.offsetHeight;
+// 헤더 시작 - 기서은 //
 
-NavMenu.forEach((item) => {
-    item.addEventListener("mouseenter", (e) => {
-        e.preventDefault();
-        item.classList.add("on");
-
-        // let SubMenuHeight = e.currentTarget.querySelector("ul").offsetHeight;
-        // let totalHeight = headerHeight + SubMenuHeight + 30;
-        // header.style.height = `${totalHeight}px`;
-
-        item.classList.add("on");
-    });
-    item.addEventListener("mouseleave", (e) => {
-        e.preventDefault();
-        item.classList.remove("on");
-
-        // header.style.height = `${headerHeight}px`;
-    });
-});
-// Nav에 마우스를 올리면 서브메뉴 나옴
-
-//스크롤시
-let scroll = 0;
-window.addEventListener("scroll", () => {
-    scroll = window.scrollY;
-    if (scroll > 0) {
-        header.classList.add("active");
-    } else {
-        header.classList.remove("active");
-    }
-    // console.log(scroll);
-});
-
-//헤더에 마우스를 올릴때 하얀색으로 바뀜
-
-header.addEventListener("mouseenter", () => {
-    if (scroll == 0) {
-        header.classList.add("active");
-    }
-    //꼭대기 일때만...
-});
-header.addEventListener("mouseleave", (e) => {
-    if (scroll == 0) {
-        header.classList.remove("active");
-    }
-    //꼭대기 일때만...
-});
-header.addEventListener("mouseleave", (e) => {
-    if (scroll == 0) {
-        header.classList.remove("active");
-    }
-});
+// 헤더 종료 - 기서은 //
 
 
-// 추천게임
+// 인기게임 시작 - 박선정 //
 
-/* 슬라이드 */
+// 인기게임 종료 - 박선정 //
+
+
+// 신규게임 시작 - 박선정 //
+
+// 신규게임 종료 - 박선정 //
+
+
+// 추천게임 시작 - 김다훈 //
+// 추천게임 - 슬라이드
 let recSlideWrapper = document.querySelector('.rec_slidewrapper'),
     recSlideContainer = recSlideWrapper.querySelectorAll('ul'),
     recHash = document.querySelectorAll('.rec_hash-list a'),
@@ -74,16 +30,18 @@ recSlideContainer.forEach(item => {
   let recSlides = item.querySelectorAll('li');
   let recSlideCount = recSlides.length;
   
-  recSlides.forEach(items => {
     item.style.width = `${recSlideWidth * recSlideCount - 10}px`;
     // 슬라이드 이동 함수 설정
     function recMoveSlide(num) {
       if (recSlideCount == 5) {
         item.style.left = `-${(2 * recSlideWidth - recSlideRemain) * num}px`;
+        recSlideBtn.style.display = 'block';
       } else if (recSlideCount == 4) {
         item.style.left = `-${(recSlideWidth - recSlideRemain) * num}px`;
+        recSlideBtn.style.display = 'block';
       } else if (recSlideCount == 3) {
         item.style.left = '71px';    // 게임 3개일 때 중앙으로
+        recSlideBtn.style.display = 'none';
       }
       recCurrentIndex = num;
     }
@@ -102,31 +60,32 @@ recSlideContainer.forEach(item => {
       e.preventDefault();
       recMoveSlide(0);
     });
-  });
-
-  // 좌우 이동 버튼 display 설정
-  if (recSlideCount == 3) {
-    recSlideBtn.style.display = 'none';
-  } else {
-    recSlideBtn.style.display = 'block';
-  }
 });
 
-// 해시태그 탭메뉴
-let recContent = document.querySelectorAll('#rec_tab > ul');
-
+// 추천게임 - 해시태그 탭메뉴
 recHash.forEach(item=> {
     item.addEventListener('click', e=> {
         e.preventDefault();
         let recTab = e.target.getAttribute('href');
         let recTabs = recTab.replace('#','');
-
-        recContent.forEach(item=> {
+        // 이전 슬라이드 안보이게 하기
+        recSlideContainer.forEach(item=> {
             item.style.display = 'none'
         })
 
-        document.getElementById(recTabs).style.display = 'block';
+        let recSlide = document.getElementById(recTabs),
+        recSlideList = recSlide.querySelectorAll('li'),
+        recSlideLen = recSlideList.length;
 
+        // 해시태그(#rec_tab-1)에서 # 제거된 ID(rec_tab-1)에 해당하는 slidewrapper 보여주기
+        recSlide.style.display = 'block';
+
+        // 게임 수 3개면 버튼 안보이게 하기
+        if(recSlideLen == 3){
+          recSlideBtn.style.display = 'none';
+        }
+
+        // 선택된 해시태그 스타일 속성 지정
         recHash.forEach(item=> {
             item.classList.remove('active');
             e.target.classList.add('active');
@@ -136,7 +95,7 @@ recHash.forEach(item=> {
 
 document.getElementById('rec_tab-1').style.display = 'block';
 
-// 모달
+// 추천게임 - 모달
 let recYoutube = document.querySelectorAll('.fa-youtube'),
     recLightbox = document.querySelector('#rec_lightbox-overlay'),
     recLightboxLink = recLightbox.querySelector('#data-lightbox'),
@@ -156,3 +115,14 @@ lightCloseBtn.addEventListener('click', ()=>{
     recLightbox.classList.remove('visible');
     recLightboxBg.classList.remove('visible');
 });
+// 추천게임 종료 - 김다훈 //
+
+
+// 이벤트&공지사항 시작 - 김아름 //
+
+// 이벤트&공지사항 종료 - 김아름  //
+
+
+// 푸터 시작 - 김아름 //
+
+// 푸터 종료 - 김아름 //
