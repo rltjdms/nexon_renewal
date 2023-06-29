b418f44bba2dfeeb82d90b31f0253d5498ec6fb0
psj0511 <137128916+psj0511@users.noreply.github.com>
2023-06-29 14:24:10 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,32 +1,3 @@
-<<<<<<< HEAD
-
-//인기게임
-/*
-변수명 tabMenu에 리스트를 할당
-*/
-let pop_tabMenu = document.querySelectorAll('.pop_tab_menu li');
-let pop_tabContent = document.querySelectorAll('#pop_tab_content .tabs')
-
-/*
-pop_tabMenu를 클릭하면 모든 pop_tabMenu에서 클래스명 active를 제거하고, 
-클릭된 그 요소에만 active를 추가한다.
-*/
-
-pop_tabMenu.forEach(i=>{
-  i.addEventListener('click',(e)=>{
-    e.preventDefault();
-    for(let list of pop_tabMenu){
-      list.classList.remove('active');
-    };
-    e.currentTarget.classList.add('active');
-    //모든 pop_tab_content는 보이지 않고, 아이디 tabs-pc 보이도록 display 속성을 block으로 변경
-    let targetId = e.target.getAttribute('href');
-    for(let content of pop_tabContent){
-      content.style.display = 'none';
-    }
-    document.querySelector(targetId).style.display = 'block';
-  });
-=======
 let header = document.querySelector("header"),
     Nav = header.querySelector("nav"),
     NavMenu = Nav.querySelectorAll("nav > ul > li");
@@ -156,7 +127,6 @@ recHash.forEach(item=> {
             e.target.classList.add('active');
         })
     })
->>>>>>> e40cd00618d4b96c38ad5c47779241fa250b7a13
 });
 
 document.getElementById('rec_tab-1').style.display = 'block';
