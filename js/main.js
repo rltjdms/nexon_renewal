0661fe069df1abbf428d3c7854d744beae8bea18
Aaaruem <137126436+Aaaruem@users.noreply.github.com>
2023-07-05 15:53:22 +0900

    Add : develop branch

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// 팝업 시작 - 김아름 //
 // 팝업창
 let popup = document.querySelector("dialog"),
   popupClose = popup.querySelector(".close"),
@@ -15,7 +16,7 @@ function setCookie(name, value, day) {
 //쿠키 가져오기
 function checkCookie(name) {
   let cookieArr = document.cookie.split(";");
-  // console.log(cookieArr)
+  console.log(cookieArr)
 
   let visited = false;
 
@@ -25,8 +26,7 @@ function checkCookie(name) {
       break;
     }
   }
-  //방문
-  // console.log(visited);
+  console.log(visited);
   if (!visited) {
     popup.setAttribute("open", "");
   }
@@ -34,152 +34,186 @@ function checkCookie(name) {
 
 //X버튼 클릭하면 닫기
 checkCookie("nexonss");
-
 popupClose.addEventListener("click", () => {
-  // console.log('닫기');
   if (dayCheck.checked) {
     setCookie("nexonss", "home", 1);
-    // console.log('쿠키생성');
   } else {
-    // setCookie("nexonss", "home", -1);
-    // console.log('쿠키닫기');
+    //setCookie("nexonss", "home", -1);
   }
   popup.removeAttribute("open");
 });
-// 팝업창 끝
+
+// 팝업 종료 - 김아름 //
+
 
 // 헤더 시작 - 기서은 //
+// 헤더 시작 - 기서은 //
 let header = document.querySelector("header"),
-  Nav = header.querySelector("nav"),
-  NavMenu = Nav.querySelectorAll("nav > ul > li");
+    Nav = header.querySelector("nav"),
+    NavMenu = Nav.querySelectorAll("nav > ul > li"),
+    //NavMenuCounter =  NavMenu.length,
+    headerTabmenu = Nav.querySelectorAll(".header_all_tab_menu > li"),
+    headerTabId = Nav.querySelectorAll(".header_all_tab_content > div");
 // headerHeight = header.offsetHeight;
 
-NavMenu.forEach((item) => {
-  item.addEventListener("mouseenter", (e) => {
-    e.preventDefault();
-    item.classList.add("on");
+NavMenu.forEach(item => {
+    item.addEventListener("mouseenter", (e) => {
+        e.preventDefault();
+        item.classList.add("on");
 
-    // let SubMenuHeight = e.currentTarget.querySelector("ul").offsetHeight;
-    // let totalHeight = headerHeight + SubMenuHeight + 30;
-    // header.style.height = `${totalHeight}px`;
+    });
+    item.addEventListener("mouseleave", (e) => {
+        e.preventDefault();
+        item.classList.remove("on");
 
-    item.classList.add("on");
-  });
-  item.addEventListener("mouseleave", (e) => {
-    e.preventDefault();
-    item.classList.remove("on");
-
-    // header.style.height = `${headerHeight}px`;
-  });
+        // header.style.height = `${headerHeight}px`;
+    });
 });
 // Nav에 마우스를 올리면 서브메뉴 나옴
+
+
+headerTabmenu.forEach((item, idx) => {
+    item.addEventListener("click", (e) => {
+        e.preventDefault();
+        // item.classList.toggle("on");
+        for(let hederTabsub of headerTabmenu) {
+          hederTabsub.classList.remove("on");
+        }
+        e.currentTarget.classList.add("on");
+
+        for (hdTabId of headerTabId) {
+            hdTabId.style.display = "none";
+            //tc.style.display = 'none';
+        }
+        //tabContent[idx].style.display ='block';
+        headerTabId[idx].style.display = "block";
+    });
+});//header tab menu click 
 
 //스크롤시
 let scroll = 0;
 window.addEventListener("scroll", () => {
-  scroll = window.scrollY;
-  if (scroll > 0) {
-    header.classList.add("active");
-  } else {
-    header.classList.remove("active");
-  }
-  // console.log(scroll);
+    scroll = window.scrollY;
+    if (scroll > 0) {
+        header.classList.add("active");
+    } else {
+        header.classList.remove("active");
+    }
+    // console.log(scroll);
 });
 
 //헤더에 마우스를 올릴때 하얀색으로 바뀜
 header.addEventListener("mouseenter", () => {
-  if (scroll == 0) {
-    header.classList.add("active");
-  }
-  //꼭대기 일때만...
+    if (scroll == 0) {
+        header.classList.add("active");
+    }
+    //꼭대기 일때만...
 });
 header.addEventListener("mouseleave", (e) => {
-  if (scroll == 0) {
-    header.classList.remove("active");
-  }
-  //꼭대기 일때만...
+    if (scroll == 0) {
+        header.classList.remove("active");
+    }
+    //꼭대기 일때만...
 });
 //배너 슬라이드
 let headerSection = document.querySelector(".header_banner"),
-  headerContent = headerSection.querySelector(".header_content"),
-  headerSlide = headerSection.querySelectorAll(".header_slide"),
-  headerpager = headerSection.querySelector(".heade_pager"),
-  headerpagerHtml = "",
-  headCounter = headerSlide.length,
-  headCurrentIndex = 0;
+    headerContent = headerSection.querySelector(".header_content"),
+    headerSlide = headerSection.querySelectorAll(".header_slide"),
+    headerVedio =  headerSection.querySelectorAll("video"),
+    headerpager = headerSection.querySelector(".heade_pager"),
+    headerpagerHtml = "",
+    headCounter = headerSlide.length,
+    headCurrentIndex = 0;
 
 if (headCounter > 0) {
-  headerSlide.forEach((list, idx) => {
-    list.style.left = `${idx * 100}%`;
-    headerpagerHtml += `<a href="">${idx}</a>`;
-  });
+    headerSlide.forEach((list, idx) => {
+        list.style.left = `${idx * 100}%`;
+        headerpagerHtml += `<a href="">${idx}</a>`;
+    });
 }
 headerpager.innerHTML = headerpagerHtml;
 let headerpagerBtn = headerpager.querySelectorAll("a");
 
 function head_moveSlide(num) {
-  headerContent.style.left = `${num * -100}%`;
-  headCurrentIndex = num;
+    headerContent.style.left = `${num * -100}%`;
+    headCurrentIndex = num;
 
-  for (let headerpg of headerpagerBtn) {
-    headerpg.classList.remove("active");
-  }
-  headerpagerBtn[headCurrentIndex].classList.add("active");
+    for (let headerpg of headerpagerBtn) {
+        headerpg.classList.remove("active");
+    }
+    headerpagerBtn[headCurrentIndex].classList.add("active");
+
+    for(let hdvd of headerVedio) {
+      hdvd.pause();
+      hdvd.currentTime = 0;
+    }
+    let headslideVideo = headerSlide[headCurrentIndex].querySelectorAll('video');
+    if (headslideVideo .length > 0) {
+       headslideVideo [0].play();
+    }
+    console.log(headslideVideo);
 }
 head_moveSlide(0);
+
 headerpagerBtn.forEach((Btn, idx) => {
-  Btn.addEventListener("click", (e) => {
-    e.preventDefault();
-    head_moveSlide(idx);
-  });
+    Btn.addEventListener("click", (e) => {
+        e.preventDefault();
+        head_moveSlide(idx);
+    });
 });
 function head_autoSlide() {
-  head_timer = setInterval(() => {
-    let headerSlideNext = (headCurrentIndex + 1) % headCounter;
-    head_moveSlide(headerSlideNext);
-  }, 28000);
-  //clearInterval(timer);
+    head_timer = setInterval(() => {
+        let headerSlideNext = (headCurrentIndex + 1) % headCounter;
+        head_moveSlide(headerSlideNext);
+    }, 28000);
+    //clearInterval(timer);
 }
 head_autoSlide();
 
+//pc방 on off
+let headRIght = document.querySelector(".header_right"),
+    headpcBtn = headRIght.querySelector(".header_onoff"); 
+
+    headpcBtn.addEventListener("click", () => {
+      headpcBtn.classList.toggle("active");
+    });
 //Quick
 let aside = document.querySelector(".quick_box"),
-  quickBtn = aside.querySelector(".quick_btt"),
-  quickList = aside.querySelector(".quick_list");
+    quickBtn = aside.querySelector(".quick_btt"),
+    quickList = aside.querySelector(".quick_list");
 
 quickBtn.addEventListener("click", () => {
-  quickBtn.classList.toggle("on");
+    quickBtn.classList.toggle("on");
 });
 // 헤더 종료 - 기서은 //
 
 // 인기게임 시작 - 박선정 //
+let pop_tabMenu = document.querySelectorAll('.pop_tab_menu li');
+let pop_tabContent = document.querySelectorAll('.pop_tab_content .tabs');
 
-/*
-변수명 tabMenu에 리스트를 할당
-*/
-let pop_tabMenu = document.querySelectorAll(".pop_tab_menu li");
-let pop_tabContent = document.querySelectorAll("#pop_tab_content > div");
+function activeTab(num){
+  for(let list of pop_tabMenu){
+    list.classList.remove('active');
+  };
+  pop_tabMenu[num].classList.add('active');
 
-/*
-pop_tabMenu를 클릭하면 모든 pop_tabMenu에서 클래스명 active를 제거하고, 
-클릭된 그 요소에만 active를 추가한다.
-*/
+  for(let content of pop_tabContent){
+    content.classList.remove('active');
+  };
+  pop_tabContent[num].classList.add('active');
+};
 
-pop_tabMenu.forEach((i) => {
-  i.addEventListener("click", (e) => {
+pop_tabMenu.forEach((item,index)=>{
+  item.addEventListener('click', e=>{
     e.preventDefault();
-    for (let list of pop_tabMenu) {
-      list.classList.remove("active");
-    }
-    e.currentTarget.classList.add("active");
-    //모든 pop_tab_content는 보이지 않고, 아이디 tabs-pc 보이도록 display 속성을 block으로 변경
-    let targetId = e.target.getAttribute("href");
-    for (let content of pop_tabContent) {
-      content.style.display = "none";
-    }
-    document.querySelector(targetId).style.display = "block";
-  });
+    activeTab(index);
+  })
 });
+
+activeTab(0);
+
+
+
 // 인기게임 종료 - 박선정 //
 
 // 신규게임 시작 - 박선정 //
@@ -187,105 +221,105 @@ pop_tabMenu.forEach((i) => {
 // 신규게임 종료 - 박선정 //
 
 // 추천게임 시작 - 김다훈 //
-let recSlideWrapper = document.querySelector(".rec_slidewrapper"),
-  recSlideContainer = recSlideWrapper.querySelectorAll("ul"),
-  recHash = document.querySelectorAll(".rec_hash-list a"),
-  recSlideWidth = 380,
-  recSlideRemain = 142, // left 이동 픽셀 설정
-  recCurrentIndex = 0, // 현재 보고있는 화면
-  recSlideBtn = document.querySelector(".rec_slide-button"),
-  recBtnLink = recSlideBtn.querySelectorAll("a"),
-  recPrevBtn = recSlideBtn.querySelector(".prev"),
-  recNextBtn = recSlideBtn.querySelector(".next");
+let recSlideWrapper = document.querySelector('.rec_slidewrapper'),
+    recSlideContainer = recSlideWrapper.querySelectorAll('ul'),
+    recHash = document.querySelectorAll('.rec_hash-list a'),
+    recSlideWidth = 380,
+    recSlideRemain = 142,      // left 이동 픽셀 설정
+    recCurrentIndex = 0,       // 현재 보고있는 화면
+    recSlideBtn = document.querySelector('.rec_slide-button'),
+    recBtnLink = recSlideBtn.querySelectorAll('a'),
+    recPrevBtn = recSlideBtn.querySelector('.prev'),
+    recNextBtn = recSlideBtn.querySelector('.next');
 
-recSlideContainer.forEach((item) => {
-  let recSlides = item.querySelectorAll("li");
+recSlideContainer.forEach(item => {
+  let recSlides = item.querySelectorAll('li');
   let recSlideCount = recSlides.length;
-
-  item.style.width = `${recSlideWidth * recSlideCount - 10}px`;
-  // 슬라이드 이동 함수 설정
-  function recMoveSlide(num) {
-    if (recSlideCount == 5) {
-      item.style.left = `-${(2 * recSlideWidth - recSlideRemain) * num}px`;
-      recSlideBtn.style.display = "block";
-    } else if (recSlideCount == 4) {
-      item.style.left = `-${(recSlideWidth - recSlideRemain) * num}px`;
-      recSlideBtn.style.display = "block";
-    } else if (recSlideCount == 3) {
-      item.style.left = "71px"; // 게임 3개일 때 중앙으로
-      recSlideBtn.style.display = "none";
+  
+    item.style.width = `${recSlideWidth * recSlideCount - 10}px`;
+    // 슬라이드 이동 함수 설정
+    function recMoveSlide(num) {
+      if (recSlideCount == 5) {
+        item.style.left = `-${(2 * recSlideWidth - recSlideRemain) * num}px`;
+        recSlideBtn.style.display = 'block';
+      } else if (recSlideCount == 4) {
+        item.style.left = `-${(recSlideWidth - recSlideRemain) * num}px`;
+        recSlideBtn.style.display = 'block';
+      } else if (recSlideCount == 3) {
+        item.style.left = '71px';    // 게임 3개일 때 중앙으로
+        recSlideBtn.style.display = 'none';
+      }
+      recCurrentIndex = num;
     }
-    recCurrentIndex = num;
-  }
-  // 해시태그 클릭할 때 마다 recMoveSlide(0) 실행
-  recHash.forEach((element) => {
-    element.addEventListener("click", () => {
+    // 해시태그 클릭할 때 마다 recMoveSlide(0) 실행
+    recHash.forEach(element => {
+      element.addEventListener('click', () => {
+        recMoveSlide(0);
+      });
+    });
+    // 슬라이드 이동 버튼 설정
+    recNextBtn.addEventListener('click', e => {
+      e.preventDefault();
+      recMoveSlide(1);
+    });
+    recPrevBtn.addEventListener('click', e => {
+      e.preventDefault();
       recMoveSlide(0);
     });
-  });
-  // 슬라이드 이동 버튼 설정
-  recNextBtn.addEventListener("click", (e) => {
-    e.preventDefault();
-    recMoveSlide(1);
-  });
-  recPrevBtn.addEventListener("click", (e) => {
-    e.preventDefault();
-    recMoveSlide(0);
-  });
 });
 
 // 추천게임 - 해시태그 탭메뉴
-recHash.forEach((item) => {
-  item.addEventListener("click", (e) => {
-    e.preventDefault();
-    let recTab = e.target.getAttribute("href");
-    let recTabs = recTab.replace("#", "");
-    // 이전 슬라이드 안보이게 하기
-    recSlideContainer.forEach((item) => {
-      item.style.display = "none";
-    });
+recHash.forEach(item=> {
+    item.addEventListener('click', e=> {
+        e.preventDefault();
+        let recTab = e.target.getAttribute('href');
+        let recTabs = recTab.replace('#','');
+        // 이전 슬라이드 안보이게 하기
+        recSlideContainer.forEach(item=> {
+            item.style.display = 'none'
+        })
 
-    let recSlide = document.getElementById(recTabs),
-      recSlideList = recSlide.querySelectorAll("li"),
-      recSlideLen = recSlideList.length;
+        let recSlide = document.getElementById(recTabs),
+        recSlideList = recSlide.querySelectorAll('li'),
+        recSlideLen = recSlideList.length;
 
-    // 해시태그(#rec_tab-1)에서 # 제거된 ID(rec_tab-1)에 해당하는 slidewrapper 보여주기
-    recSlide.style.display = "block";
+        // 해시태그(#rec_tab-1)에서 # 제거된 ID(rec_tab-1)에 해당하는 slidewrapper 보여주기
+        recSlide.style.display = 'block';
 
-    // 게임 수 3개면 버튼 안보이게 하기
-    if (recSlideLen == 3) {
-      recSlideBtn.style.display = "none";
-    }
+        // 게임 수 3개면 버튼 안보이게 하기
+        if(recSlideLen == 3){
+          recSlideBtn.style.display = 'none';
+        }
 
-    // 선택된 해시태그 스타일 속성 지정
-    recHash.forEach((item) => {
-      item.classList.remove("active");
-      e.target.classList.add("active");
-    });
-  });
+        // 선택된 해시태그 스타일 속성 지정
+        recHash.forEach(item=> {
+            item.classList.remove('active');
+            e.target.classList.add('active');
+        })
+    })
 });
 
-document.getElementById("rec_tab-1").style.display = "block";
+document.getElementById('rec_tab-1').style.display = 'block';
 
 // 추천게임 - 모달
-let recYoutube = document.querySelectorAll(".fa-youtube"),
-  recLightbox = document.querySelector("#rec_lightbox-overlay"),
-  recLightboxLink = recLightbox.querySelector("#data-lightbox"),
-  recLightboxBg = document.querySelector("#rec_lightbox-background"),
-  lightCloseBtn = document.querySelector("#rec_lightbox-close");
+let recYoutube = document.querySelectorAll('.fa-youtube'),
+    recLightbox = document.querySelector('#rec_lightbox-overlay'),
+    recLightboxLink = recLightbox.querySelector('#data-lightbox'),
+    recLightboxBg = document.querySelector('#rec_lightbox-background'),
+    lightCloseBtn = document.querySelector('#rec_lightbox-close');
 
-recYoutube.forEach((item) => {
-  item.addEventListener("click", (e) => {
-    e.preventDefault();
-    let linkSrc = item.getAttribute("data-lightbox");
-    recLightboxLink.setAttribute("src", linkSrc);
-    recLightbox.classList.add("visible");
-    recLightboxBg.classList.add("visible");
-  });
+recYoutube.forEach(item =>{
+    item.addEventListener('click', e=>{
+        e.preventDefault();
+        let linkSrc = item.getAttribute('data-lightbox');
+        recLightboxLink.setAttribute('src',linkSrc);
+        recLightbox.classList.add('visible');
+        recLightboxBg.classList.add('visible');
+    });
 });
-lightCloseBtn.addEventListener("click", () => {
-  recLightbox.classList.remove("visible");
-  recLightboxBg.classList.remove("visible");
+lightCloseBtn.addEventListener('click', ()=>{
+    recLightbox.classList.remove('visible');
+    recLightboxBg.classList.remove('visible');
 });
 // 추천게임 종료 - 김다훈 //
 
@@ -293,7 +327,7 @@ lightCloseBtn.addEventListener("click", () => {
 let eventWrapper = document.querySelector(".event"), // event(부모)
   eventSlide = eventWrapper.querySelector(".event-slide"), //event ul
   eventslideList = eventSlide.querySelectorAll("li"),
-  // 각 슬라이드 갯수
+  // 각 슬라이드
   dotPager = eventWrapper.querySelector(".event-dot"), //dot버튼
   dotIndex = "", //닷버튼 공간
   eventTimer, //이벤트 시간
@@ -302,13 +336,11 @@ let eventWrapper = document.querySelector(".event"), // event(부모)
   currentSlideIdx = 0; // 보고있는 이벤트 화면
 
 if (slideCount > 1) {
-  eventslideList.forEach((item, idx) => { //슬라이드 이미지 반복해서 보여주기
+  eventslideList.forEach((item, idx) => {
     item.style.left = `${idx * 100}%`;
     dotIndex += `<a href="">${idx}</a>`; //dot버튼 생성
   });
 }
-
-//닷버튼
 dotPager.innerHTML = dotIndex;
 let dotbtn = dotPager.querySelectorAll("a");
 function eventmoveSlide(num) {
@@ -324,14 +356,13 @@ function eventmoveSlide(num) {
   }
   dotbtn[currentSlideIdx].classList.add("active");
 }
-eventmoveSlide(0); //첫번째 화면 보여주기
+eventmoveSlide(0);
 dotbtn.forEach((item, idx) => {
-  item.addEventListener("click", (e) => { //여기서 e는 닷버튼
-    e.preventDefault(); //a 태그를 누르면 링크로 이동하는데 그걸 방지하기 위한 동작
-    eventmoveSlide(idx); //idx 페이지로 이동
+  item.addEventListener("click", (e) => {
+    e.preventDefault();
+    eventmoveSlide(idx);
   });
 });
-
 // 이벤트 슬라이드 루프 시작하기
 function event_autoSlide() {
   eventTimer = setInterval(() => {
@@ -350,6 +381,33 @@ eventWrapper.addEventListener("mouseleave", () => {
 });
 // 이벤트&공지사항 종료 - 김아름  //
 
+// 퀵메뉴 시작 - 기서은 //
+window.addEventListener("scroll", () => {
+  popGame= document.querySelector('.pop_tab_wrapper');
+  popGameTop = popGame.offsetTop;
+  scroll = window.scrollY;
+  ftGame= document.querySelector('footer');
+  ftGameTop = ftGame.offsetTop;
+  console.log(popGameTop);
+
+  if (scroll > popGameTop - 500 && scroll < ftGameTop - 500) {
+    aside.classList.remove("fixed");
+  } else {
+    aside.classList.add("fixed");
+  }
+});
+// 퀵메뉴 종료 - 기서은 //
+
 // 푸터 시작 - 김아름 //
 
 // 푸터 종료 - 김아름 //
+
+// 다크모드 시작 //
+let darkcheck = document.querySelector(".quick_box");
+let darkcheckbox = darkcheck.querySelector("#quick_toggle");
+let darkBtn = darkcheck.querySelector("#darkBtn");
+let body = document.querySelector("body");
+darkBtn.addEventListener("click", ()=> {
+    body.classList.toggle("dark-active");
+  });
+// 다크모드 종료 //
